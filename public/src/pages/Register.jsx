964f19b1361018d7876b74d05130a637462eacf6
Priyanshu <myselfpriyanshu1@gmail.com>
2024-01-27 18:57:12 +0530

    server files added

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,25 +1,59 @@
 import React,{useState,useEffect} from "react";
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
-import Logo from "../assets/logo.svg"
-
+import Logo from "../assets/logo.svg";
+import {ToastContainer , toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 function Register() {
     const [values, setValues] = useState({
         username: "",
         email: "",
-        passowrd: "",
+        password: "",
         confirmPassword: "",
     });
-    const handleSubmit =(event)=>{
+    const toastOptions={
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    }
+    const handleSubmit =async (event)=>{
         event.preventDefault();
-        alert("form");
+        if(handleValidation()){
+        const { password, confirmPassword, username, email } = values;   
+            const {data} = await axios.post(registerRoute,{
+                username,
+                email,
+                password,
+            });
+        }
     };
 
-    const handleValidation = ()=>{
-        const {password, confirmPassword, username, email} = values;
-        if (password !== confirmPassword){
-
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+          toast.error(
+            "Password and confirm password should be the same.",
+             toastOptions
+             );
+          return false;
+        } else if (username.length < 3) {
+          toast.error("Username should be greater than 3 characters.", toastOptions);
+          return false;
+        } else if (password.length < 8) {
+          toast.error(
+            "Password should be equal or greater than 8 characters.", 
+            toastOptions
+            );
+            return false;
+        }else if(email ===""){
+            toast.error("email is required",toastOptions);
+            return false;
         }
+        return true;
     };
 
     const handleChange = (event) => {
@@ -54,7 +88,7 @@ function Register() {
             <input
             type="password"
             placeholder="Confirm Password"
-            name="conifrmPassword"
+            name="confirmPassword"
             onChange={(e) => handleChange(e)}
             />
             <button type="submit" >Create User</button>
@@ -62,6 +96,7 @@ function Register() {
             </span>
             </form>
     </FormContainer> 
+    <ToastContainer/>
     </>
     );
   
